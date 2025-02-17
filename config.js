4a922b06f7e91660dc0d13994971034ee2c37ba3
Taweret23 <2gianthamsters@gmail.com>
2025-02-17 01:10:43 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-const firebaseConfig = { apiKey: "AIzaSy" + "BmN2siq" + "nx3inX1UA" + "3JaA8Ng8Ebf" + "CRQcao
 
-", authDomain: "star-wars-or-baseball.firebaseapp.com", projectId: "star-wars-or-baseball", storageBucket: "star-wars-or-baseball.appspot.com", messagingSenderId: "578105943516", appId: "1:578105943516:web:1a23e14116694499fb5b19" };
