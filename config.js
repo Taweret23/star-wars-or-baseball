b550c431ce2a7c33f2776aad9ca1c91ebc9d8209
Taweret23 <2gianthamsters@gmail.com>
2025-02-17 00:41:20 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJofXAdyXttFjQyUF0IkClRoJJGCSB7Pw",  
+    authDomain: "star-wars-or-baseball.firebaseapp.com",
+    projectId: "star-wars-or-baseball",
+    storageBucket: "star-wars-or-baseball.appspot.com",
+    messagingSenderId: "578105943516",
+    appId: "1:578105943516:web:1a23e14116694499fb5b19"
+};
